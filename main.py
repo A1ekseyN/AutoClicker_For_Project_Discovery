b4b726161ca07844ab157ab110f141f6757f14bd
A1ekseyN <91666956+A1ekseyN@users.noreply.github.com>
2022-12-15 12:57:21 +0100

    Update main.py
    - Version - 0.0.3b
    - Add ti GitHub

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
 # AutoClicker for Project Discovery in Eve Online
-# Version - 0.0.3a
-# Работает ноутбуке, с персонажами: Allehandro, Anna de Amarr, Elise Jackson, Suzanne Jackson, Blondie Loo
+# Version - 0.0.3b
 
 # Omega clone - 200 заданий в день
 # Alpha clone - 100 заданий в день (Нужно уточнить число)
 
 # Tasks (Фичи):
-# Дописать остановку выполнения скрипта
+# Дописать остановку/паузу выполнения скрипта по хот-кею
+# Сделать, чтобы скрипт в конце не закрывался, а показывал результат работы
+# Каталог аккаунтов, с информацией о количестве доступных на сегодня заданий
 # Возможность по хоткею добавлять таски в задания
 # GUI интерфейс (Не понятно или он сейчас нужен)
 # Функционал, который запоминает время обнуления Project Discovery
+
 
 # Идеи для бота:
 # Авто-подскан каждые 3-5 секунд, по горячей кнопке.
 # Добавить клик по кнопке перехода через гипер-врата. (Скорее всего активируется отдельно, через команду if)
 
-# 1. Allehandro - приверно - 14.29 (21.03.2022)
+# 1. Allehandro - 99
 # 1. Anna de Amarr - Нет 1.000.000 skill point
 # 2. Elise Jackson - 2Do
 # 2. Suzane Jackson - < 914k exp
 # 3. Blondie Loo - 2Do
 
 import time
-import threading
-import pynput
 from pynput.mouse import Button, Controller
 
 from datetime import timedelta
@@ -35,20 +35,27 @@ mouse = Controller()
 # Там где показан счёт. То, есть по сути 1 проект дисковери нужно пройти.
 #####################################
 
-counter = input("Сколько циклов выполнить? ")
-counter = int(counter)
+
+def counter_iteration():
+    global counter
+    try:
+        counter = int(input("Сколько циклов выполнить?\n>>> "))
+    except:
+        print('Лучше ввести число.')
+        counter_iteration()
+
+counter_iteration()
 
 # Characters: // Allehandor - 0 (???)   // Anna de Amarr --- // Elise Jackson - 66 // Suzane Jackson --- // Blondie Loo - 10
 # timestamp:  //time = 14.48 (22) //       ---         // time = 13.29 (17)  // time = 15.55 (02)   //  time = 16.00 (22)
 
 isk_for_task = (counter * 99000)
-
 task_time_one = timedelta(minutes=0, seconds=12.356)
 task_time_total = task_time_one * counter
 
-
 print(f"Предположительный доход за {counter} заданий {isk_for_task:,.0f} isk.")
 print(f"Время на выполнение задания: {task_time_total}.")
+
 
 def timer_start():
     print("\nСтарт через 7 секунд.")
@@ -67,7 +74,6 @@ def timer_start():
     time.sleep(1)
     print("Старт!")
 
-# Вызов функции таймера перед стартом. На данный момент таймер настроен на 7 секунд.
 timer_start()
 
 # Start timer
@@ -77,12 +83,9 @@ mouse.position = (0, 0)
 print("Corrent position: " + str(mouse.position))
 
 
-##### Start Цикла While #####
-# Цикл прохождения задания в Project Discovery.
-
 def project_discovery(counter=counter):
+    # Цикл прохождения задания в Project Discovery.
     while counter > 0: # Счётчик цикла
-
         ##### Start Discovery Project #####
         time.sleep(1)
         mouse.position = (1248, 771)
@@ -149,16 +152,13 @@ def project_discovery(counter=counter):
         mouse.click(Button.left, 1)
 
         # Конец цикла и одного задания Project Discovery
-        print(f"Осталось {counter} циклов.")
-        print("--- %s seconds ---" % (time.time() - start_time))
+        print(f"\nОсталось: {counter} циклов.")
+        print(f'Время выполнения скрипта: {time.time() - start_time:,.2f} сек.')
 
         counter -= 1
 
-# Выполнение заданий Project Discovery.
 project_discovery()
 
 
-print("Конец выполнения программы.")
-print("--- %s seconds ---" % (time.time() - start_time))
-
-
+print("\n--- Конец выполнения программы. ---")
+print(f'Выполнено за: {time.time() - start_time:,.2f} сек.')
