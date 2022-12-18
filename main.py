5f19006a544435d4ddb7356008b0812215beb915
A1ekseyN <lolik4ever@rambler.ru>
2022-12-18 12:45:46 +0200

    - Test Commit & Push

--- main.py
+++ main.py
@@ -1,42 +1,16 @@
 # AutoClicker for Project Discovery in Eve Online
-# Version - 0.0.3e
+# Version - 0.0.3g
 
-# Omega clone - 200 заданий в день
-# Alpha clone - 100 заданий в день (Нужно уточнить число)
-
-# Tasks (Фичи):
-# Дописать остановку/паузу выполнения скрипта по хот-кею
-# Сделать, чтобы скрипт в конце не закрывался, а показывал результат работы
-# Каталог аккаунтов, с информацией о количестве доступных на сегодня заданий
-# Возможность по хоткею добавлять таски в задания
-# GUI интерфейс (Не понятно или он сейчас нужен)
-# Функционал, который запоминает время обнуления Project Discovery
-
-
-# Идеи для бота:
-# Авто-подскан каждые 3-5 секунд, по горячей кнопке.
-# Добавить клик по кнопке перехода через гипер-врата. (Скорее всего активируется отдельно, через команду if)
-
-# 1. Allehandro - 99
-# 1. Anna de Amarr - Нет 1.000.000 skill point
-# 2. Elise Jackson - 2Do
-# 2. Suzane Jackson - < 914k exp
-# 3. Blondie Loo - 2Do
 
 import time
 from pynput.mouse import Button, Controller
-
 from datetime import timedelta
 
 mouse = Controller()
 
-#####################################
-# !!! Start - стартовать нужно с главного окна Project Discovery.
-# Там где показан счёт. То, есть по сути 1 проект дисковери нужно пройти.
-#####################################
-
 
 def counter_iteration():
+    # Запрос на кол-во циклов.
     global counter
     try:
         counter = int(input("Сколько циклов выполнить?\n>>> "))
@@ -46,8 +20,6 @@ def counter_iteration():
 
 counter_iteration()
 
-# Characters: // Allehandor - 0 (???)   // Anna de Amarr --- // Elise Jackson - 66 // Suzane Jackson --- // Blondie Loo - 10
-# timestamp:  //time = 14.48 (22) //       ---         // time = 13.29 (17)  // time = 15.55 (02)   //  time = 16.00 (22)
 
 isk_for_task = (counter * 99000)
 task_time_one = timedelta(minutes=0, seconds=12.356)
@@ -58,6 +30,7 @@ print(f"Время на выполнение задания: {task_time_total}."
 
 
 def timer_start():
+    # Функция обратного отчета перед стартом выполнения задания.
     print("\nСтарт через 7 секунд.")
     time.sleep(1)
     print("6")
@@ -76,7 +49,6 @@ def timer_start():
 
 timer_start()
 
-# Start timer
 start_time = time.time()
 
 mouse.position = (0, 0)
